129705f8156d966359900b8428dea9d88ad38fe6
Karina Prado <karina.pradogutierrez@gmail.com>
2023-04-09 17:46:38 -0500

    Feat: update methods

---
+++ challenge_01/README.md
@@ -0,0 +1,7 @@
+### Challenge 1
+
+*Create Class ProductManager
+*Create methods:
+-getProducts()
+-getProductById(id)
+*Instantiate a class and show examples
--- challenge_01/index.js
+++ challenge_01/index.js
@@ -12,10 +12,30 @@ class ProductManager {
             let lastProduct = this.products[this.products.length - 1];
             id = lastProduct.id + 1;
         }
-
-        let product = { id, title, description, price, thumbnail, stock}
-        this.products.push(product);
+        let existingProduct = (this.products.find(product => product.title == title));
+        
+        if(existingProduct) {
+            console.log("You cannot create an existing product");
+        }else{
+            let product = { id, title, description, price, thumbnail, stock}
+            this.products.push(product);
+        }
     }
+
+    // addProduct(item) {
+    //     let id = 0;
+    //     if(this.products.length === 0) {
+    //         id = 1;
+    //     }else {
+    //         let lastProduct = this.products[this.products.length - 1];
+    //         id = lastProduct.id + 1;
+    //     }
+
+    //     let product = { id, ...item };
+    //     console.log(product, 'product');
+    //     this.products.push(product);
+    // }
+
     getProducts() {
         return this.products;
     }
@@ -33,6 +53,7 @@ let product = new ProductManager();
 
 product.addProduct({ title: 'Iphone', description: 'High quality', price: 4500, thumbnail: '', stock: 10})
 product.addProduct({ title: 'Macbook', description: 'High quality', price: 7500, thumbnail: '', stock: 20})
+product.addProduct({ title: 'Ipad', description: 'High quality', price: 3500, thumbnail: '', stock: 5})
 
 console.log(product.getProducts());
 console.log("--------------------");
