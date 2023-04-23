f1798cf48e21379039ef7276c567f912a71ae0a3
Karina Prado <karina.pradogutierrez@gmail.com>
2023-04-23 11:58:05 -0500

    Feat: add challenge 2 setup

---
+++ challenge_02/README.md
@@ -0,0 +1,12 @@
+### Challenge 2
+
+- Create Class ProductManager
+
+- Create methods:
+addProduct()
+getProducts(id)
+getProductById(id)
+updateProduct(id, data)
+deleteProduct(id)
+
+- Instantiate a class and show examples
---
+++ challenge_02/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+class ProductManager {
+
+    constructor(path) {
+        this.products = [];
+        this.path = path;
+    }
+
+    async incrementId() {
+    }
+
+    async addProduct(product) {
+    }
+    async getProducts() {
+    }
+    async getProductById(id) {
+    }
+    async updateProduct(id, data) {
+    }
+    async deleteProduct(id) {
+    }
+}
