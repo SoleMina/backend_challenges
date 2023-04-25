0e0c9be2c628ef86d31e608a5725e9b2d2d5ff79
Karina Prado <karina.pradogutierrez@gmail.com>
2023-04-25 16:42:59 -0500

    Feat: update methods

--- challenge_02/index.js
+++ challenge_02/index.js
@@ -11,30 +11,55 @@ class ProductManager {
         let id = 0;
         if(this.products.length === 0) {
             id = 1;
-            return id;
         }else {
-            let lastProduct = this.products[this.products.length - 1];
+            const lastProduct = this.products[this.products.length - 1];
             id = lastProduct.id + 1;
-            return id;
         }
+        return id;
     }
 
     async addProduct(product) {
-        //verifico si existe el archivo
+        //Check if file exists
         let file = fs.existsSync(this.path);
         try {
             if(file) {
-                //si existe cargo los usuarios en la memoria del programa
+                console.log(file, 'file');
+                //If file existe read json
                 this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
                 console.log(this.products, "this.products");
 
-                //If exist product with the same name
+                //Check if there are products with the same name
                 let prod = this.products.find( pd => pd.title === product.title);
-                console.log(prod, "prod");
 
                 if(prod) {
                     return {status: "error", message: "Product already exist"}
                 }else{
+                    try {
+                        //Id increment
+                        let id = await this.incrementId();
+
+                        let dataObj = {
+                            id: id,
+                            title: product.title,
+                            description: product.description,
+                            price: product.price,
+                            thumbnail: product.thumbnail,
+                            code: product.code,
+                            stock: product.stock
+                        }
+                        this.products.push(dataObj);
+                    
+                        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
+                        return {status: "Success", message: "Product created successfully!"}
+                    } catch (error) {
+                        return {status: "error", message: `Cannot create product here ${prod}`}
+                    }
+
+                }
+            }else{
+                //If file doesn't exist, create it
+
+                try {
                     //Id increment
                     let id = await this.incrementId();
 
@@ -47,53 +72,29 @@ class ProductManager {
                         code: product.code,
                         stock: product.stock
                     }
-                    this.products.push(dataObj);
 
-                    try {
-                        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
-                        return {status: "Success", message: "Product created successfully!"}
-                    } catch (error) {
-                        return {status: "error", message: `Cannot create product ${prod}`}
-                    }
-
-                }
-            }else{
-                //El archivo no existe, entonces hay que crearlo
-
-                //Id increment
-                let id = await this.incrementId();
-
-                let dataObj = {
-                    id: id,
-                    title: product.title,
-                    description: product.description,
-                    price: product.price,
-                    thumbnail: product.thumbnail,
-                    code: product.code,
-                    stock: product.stock
-                }
-                try {
                     await fs.promises.writeFile(this.path, JSON.stringify([dataObj], null, 2));
                     return {status: "success", message: "Product created successfully!"}
+
                 } catch (error) {
-                    return {status: "error", message: "Cannot create product" + error}
+                    return {status: "error", message: "Cannot create product: " + error}
                 }
 
             }
         } catch (error) {
-            return {status: "error", message: "Cannot create product" + error}
+            return {status: "error", message: "Cannot create product: " + error}
         }
     }
     async getProducts() {
         try {
             let products  = await fs.promises.readFile(this.path, "utf-8");
-            if(!products) return {status: "error", message: "Cannot create product" + error};
+            if(!products) return {status: "error", message: "Products not found: " + error};
 
             this.products = JSON.parse(products);
-            return {status: "success", products: this.products, message: "Products"}
+            return {status: "success", products: this.products, message: "List of products"}
 
         } catch (error) {
-            return {status: "error", message: "Cannot create product" + error}
+            return {status: "error", message: "Products not found: " + error}
         }
     }
     async getProductById(id) {
@@ -102,12 +103,12 @@ class ProductManager {
             this.products = JSON.parse(products);
             
             let product = this.products.find(prd => prd.id === id);
-            if(!product) return {status: "error", message: "Product not found"}
+            if(!product) return {status: "error", message: "Product not found: " + error}
 
             return {status: "Success", product: product, message: "Product found successfully!"}
             
         } catch (error) {
-            return {status: "error", message: "Cannot find product" + error}
+            return {status: "error", message: "Product not found: " + error}
         }
     }
     async updateProduct(id, data) {
@@ -119,15 +120,15 @@ class ProductManager {
             console.log(this.products);
 
             if(index>=0) {
-                this.products[index] ={ id, ...data}
+                this.products[index] = { id, ...data}
                 await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
                 return {status: "Success", product: this.products[index], message: "Product updated successfully!"}
             }else{
-                return {status: "error", message: "Product not found, can't update"}
+                return {status: "error", message: "Product not found, can't update: " + error}
             }
             
         } catch (error) {
-            return {status: "error", message: "Cannot update product" + error}
+            return {status: "error", message: "Cannot update product: " + error}
         }
     }
     async deleteProduct(id) {
@@ -142,34 +143,41 @@ class ProductManager {
                 await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
                 return {status: "success", message: "Product has been deleted"}
             }else{
-                return {status: "error", message: "Cannot eliminate product"}
+                return {status: "error", message: "Cannot delete product: " + error}
             }
 
         } catch (error) {
-            return {status: "error", message: "Cannot eliminate product" + error}
+            return {status: "error", message: "Cannot delete product: " + error}
         }
     }
 }
 
-let product = new ProductManager("./data/products.json");
+(async () => {
+    let product = new ProductManager("./data/products.json");
 
-// product.addProduct({ title: 'Macbook', description: 'High quality', price: 4500, thumbnail: '', code: '000A1', stock: 10})
-//   .then(result => {
-//     console.log(result.message);
-// })
-// product.getProducts().then(result => {
-//     console.log(result.products);
-//     console.log(result.message);
-// });
-// product.getProductById(1).then(result => {
-//     console.log(result.product);
-//     console.log(result.message);
-// });
-// product.updateProduct(2, { title: 'Macbook', description: 'High quality', price: 4500, thumbnail: '', code: '000A1', stock: 10})
-//   .then(result => {
-//     console.log(result.message);
-//     console.log(result.product);
-// });
-product.deleteProduct(1).then(result => {
-    console.log(result.message);
-});
+    await product.addProduct({ title: 'Macbook', description: 'High quality', price: 4500, thumbnail: '', code: '000A1', stock: 10})
+      .then(result => {
+        console.log(result.message);
+    });
+    await product.addProduct({ title: 'Ipad', description: 'High quality', price: 4500, thumbnail: '', code: '000A1', stock: 10})
+      .then(result => {
+        console.log(result.message);
+    });
+    await product.getProducts().then(result => {
+        console.log(result.products);
+        console.log(result.message);
+    });
+    await product.getProductById(1).then(result => {
+        console.log(result.product);
+        console.log(result.message);
+    });
+    // await product.updateProduct(1, { title: 'Macbookcito', description: 'High quality', price: 4500, thumbnail: '', code: '000A1', stock: 10})
+    //   .then(result => {
+    //     console.log(result.message);
+    //     console.log(result.product);
+    // });
+    // await product.deleteProduct(2).then(result => {
+    //     console.log(result.message);
+    // });
+
+})();
