920466636ba29f6165264e55bf5f5fd650e53f0c
Karina Prado <karina.pradogutierrez@gmail.com>
2023-04-29 18:32:57 -0500

    Feat: install express & server

--- clases/Servidores/index.js
+++ clases/Servidores/index.js
@@ -10,11 +10,51 @@ let ready = () => console.log("Server ready on port " + PORT);
 server.listen(PORT, ready);
 
 server.use(express.urlencoded({extended:true}));
+server.use(express.json());
 
 let index_route = "/";
 let index_function = (req, res) => {
-    let quantity = manager.read_user().length;
+    let quantity = manager.read_users().length;
     return res.send(`There are ${quantity} users`);
 }
 
 server.get(index_route, index_function);
+
+let one_route = "/users/:id";
+let one_function = (req, res) => {
+    let {id} = req.params;
+    id = Number(id);
+    let one = manager.read_user(id);
+    if(one) {
+        return res.send({
+            success: true,
+            user: one
+        });
+    }else {
+        return res.send({
+            success: false,
+            user: "Not found"
+        });
+    }
+}
+server.get(one_route, one_function);
+
+let query_route = "/users";
+//let query_route = "/users?quantity=2";    query
+let query_function = (req, res) => {
+    let quantity = req.query.quantity ?? 5;
+    let users = manager.read_users().slice(0, quantity);
+    if(users.length > 0) {
+        return res.send({
+            success: true, 
+            users
+        })
+    }else {
+        return res.send({
+            success: false, 
+            users: 'Not found'
+        })
+    }
+};
+
+server.get(query_route, query_function);
