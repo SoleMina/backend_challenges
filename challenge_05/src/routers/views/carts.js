db6011ae044b5c7340e6c8805898ce27e67a3512
Karina Prado <karina.pradogutierrez@gmail.com>
2023-05-16 20:22:38 -0500

    Feat: add carts view

--- challenge_05/src/routers/views/carts.js
+++ challenge_05/src/routers/views/carts.js
@@ -1,15 +1,27 @@
 import { Router} from "express";
+import carts from "../../classes/cart.js";
 
 const router = Router();
 
 router.get("/", async (req, res, next) => {
     try {
-        return res.render(
-          "carts", 
-          {
-            title: "Carts"
-          }
-        );
+        let id = Number(1);
+        if(id) {
+            const obj = (await carts.getCartById(id)).cartId;
+            return res.render(
+                "carts", 
+                {
+                  title: "Carts",
+                  carts: obj
+                }
+              );
+        }else {
+            return res.status(400).json({
+                success: false,
+                message: "Cart not found"
+            })
+        }
+        
     } catch (error) {
         next(error);
     }
