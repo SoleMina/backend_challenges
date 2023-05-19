5a32f4e862cc8f36f6c335cbae82043911fa1bb5
Karina Prado <karina.pradogutierrez@gmail.com>
2023-05-19 16:04:51 -0500

    Feat: update

--- challenge_05/src/middlewares/multer.js
+++ challenge_05/src/middlewares/multer.js
@@ -2,14 +2,7 @@ import multer from 'multer';
 import __dirname from '../utils.js';
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
-        if(file.fieldname==="image"){
-            cb(null,__dirname+'/public/images');
-        }else if(file.fieldname==="documents"){
-            cb(null,'documents');
-        }
-        else{
-            cb(null,'uploads');
-        }
+        cb(null,__dirname+'/public/images');
     },
     filename:(req,file,cb)=>{
         cb(null,Date.now()+file.originalname);
