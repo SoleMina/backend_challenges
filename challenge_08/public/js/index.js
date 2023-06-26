668b32eab25f40e26ce33f4dea18b312b8fbf6fa
Karina Prado <karina.pradogutierrez@gmail.com>
2023-06-25 19:13:51 -0500

    Feat: update

--- challenge_08/public/js/index.js
+++ challenge_08/public/js/index.js
@@ -3,23 +3,10 @@ const socket = io();
 let count = Number(document.getElementById("count").textContent);
 let buy = document.getElementById("buy");
 
-// buy.addEventListener("click", () => {
-//     count = count + 1;
-//     document.getElementById("count").innerHTML = count;
-// });
+buy.addEventListener("click", () => {
+    count = count + 1;
+    document.getElementById("count").innerHTML = count;
+});
 
 
 /***** *****/
-
-document.getElementById('signout').addEventListener('click',(event)=>{
-    event.preventDefault();
-
-    fetch(`/api/auth/signout`, {
-        method: 'POST',
-    })
-        .then(res=>res.json())
-        .then(res=>console.log(res))    //en lugar de imprimir en consola: mostrar mensaje de alerta
-        .then(res => location.href = '/login')
-        .catch(err=>console.log(err))   //en lugar de imprimir en consola: mostrar mensaje de alerta
-})
-
