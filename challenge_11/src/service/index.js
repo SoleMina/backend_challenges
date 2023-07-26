d579a01749e8f42a34428d2838a46c3fd029af1d
Karina Prado <karina.pradogutierrez@gmail.com>
2023-07-26 15:22:57 -0500

    Feat: add controller cart

--- challenge_11/src/service/index.js
+++ challenge_11/src/service/index.js
@@ -1,5 +1,7 @@
 import ProductDaoMongo from "../dao/Mongo/products.mongo.js";
 import UserDaoMongo from "../dao/Mongo/users.mongo.js";
+import CartDaoMongo from "../dao/Mongo/cart.mongo.js";
 
 export const productService = new ProductDaoMongo();
 export const userService = new UserDaoMongo();
+export const cartService = new CartDaoMongo();
