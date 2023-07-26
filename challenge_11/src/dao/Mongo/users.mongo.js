fcc9986e998198e4e6969a0ecca2903e096f60c0
Karina Prado <karina.pradogutierrez@gmail.com>
2023-07-25 23:47:32 -0500

    Feat: add controller & model

--- challenge_11/src/dao/Mongo/users.mongo.js
+++ challenge_11/src/dao/Mongo/users.mongo.js
@@ -4,9 +4,6 @@ class UserDaoMongo {
     constructor() {
         this.userModel = User;
     }
-
-    registerUser = (req, res) => res.status(201).json({
-        success: true,
-        message: "User created!"
-    });
 }
+
+export default UserDaoMongo;
