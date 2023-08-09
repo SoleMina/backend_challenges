ffe8c3eb513a9ea114020a2122d39a243ebcc70b
Karina Prado <karina.pradogutierrez@gmail.com>
2023-08-09 13:35:40 -0500

    Feat: add send gmail config

--- challenge_12/src/routers/api/index.js
+++ challenge_12/src/routers/api/index.js
@@ -4,6 +4,7 @@ import authRouter from "./auth.router.js";
 import productsRouter from "./products.router.js";
 import cartsRouter from "./carts.mongo.js";
 import registerRouter from "./carts.memory.js";
+import {sendMail} from "../../utils/sendMail.js";
 
 const router = Router();
 
@@ -11,5 +12,14 @@ router.use("/products", productsRouter);
 router.use("/carts", cartsRouter);
 router.use('/register',registerRouter);
 router.use('/auth',authRouter);
+
+router.get("/mail", async(req, res, next) => {
+    try {
+        await sendMail();
+        res.send("Email enviado");
+    } catch (error) {
+       console.log(error);
+    }
+});
 
 export default router;
