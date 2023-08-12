22bf0b4983b3106c3a9bdf5493b47b80c4238b75
Karina Prado <karina.pradogutierrez@gmail.com>
2023-08-11 23:16:26 -0500

    Feat: add user path with custom error

--- challenge_13/src/controllers/user.controller.js
+++ challenge_13/src/controllers/user.controller.js
@@ -9,27 +9,31 @@ class UserController {
         this.userService = userService;
     }
     registerUser = async (req, res) => {
-        
-        let {name, photo, email, age, rol, password} = req.body;
 
-        if(!name || email || password) {
-            CustomError.createError({
-                name: "User Creation error",
-                cause: generateUserErrorsInfo({name, email, password}),
-                message: "Error trying to create user",
-                code: EErrors.INVALID_TYPE_ERROR
+        try {
+            let {name, photo, email, age, rol, password} = req.body;
+    
+            if(!name || !email || !password) {
+                CustomError.createError({
+                    name: "User Creation error",
+                    cause: generateUserErrorsInfo({name, email, password}),
+                    message: "Error trying to create user",
+                    code: EErrors.INVALID_TYPE_ERROR
+                });
+            }
+    
+            let newUser = new UserDTO({name, photo, email, age, rol, password});
+    
+            let result = this.service.create(newUser);
+                    
+            return res.status(201).json({
+                success: true,
+                message: "User created!",
+                payload: {name, email, age}
             });
+        } catch (error) {
+            next(error);
         }
-
-        //let newUser = new UserDTO({name, photo, email, age, rol, password});
-
-        // let result = this.service.create(newUser);
-                
-        return res.status(201).json({
-            success: true,
-            message: "User created!",
-            payload: {name, email, age}
-        });
     }
     signIn = async(req, res, next) => {
         try {
